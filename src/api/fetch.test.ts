19ac1b2ac1587364d23a9e638afe69044fb6b283
Patrycja <patrycja.krzeszowiec@synergycodes.com>
2020-08-11 19:27:49 +0200

    added test for mocked axios get

--- src/api/fetch.test.ts
+++ src/api/fetch.test.ts
@@ -3,7 +3,25 @@ import axios from 'axios';
 import { getGhibliMovies } from './fetch';
 
 jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-describe('fetchData', () => {
+describe('fetch Data from API', () => {
 
+    it('fetches successfully data from an API', async () => {
+        const fetchedData = {
+            data: [{
+                title: 'Castle in the Sky'
+            }]
+        };
+        mockedAxios.get.mockImplementationOnce(() => Promise.resolve(fetchedData));
+        const result = await getGhibliMovies();
+        expect(result).toEqual(['Castle in the Sky']);
+    });
+
+    it('fetches erroneously data from an API', async () => {
+        const errorMessage = 'Fetch Error';
+
+        mockedAxios.get.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
+        await expect(getGhibliMovies()).rejects.toThrow(errorMessage);
+    });
 });
--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -4,9 +4,9 @@ export type IFilm = {
     id: string;
     title: string;
 }
+
 export const getGhibliMovies = async() => axios.get('https://ghibliapi.herokuapp.com/films')
     .then(({ data }) => data.map((item: IFilm) => item.title))
     .catch((error) => {
-        console.log(error);
-        return [];
+        throw error;
     });
